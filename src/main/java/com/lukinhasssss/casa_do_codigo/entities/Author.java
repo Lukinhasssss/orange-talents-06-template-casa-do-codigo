c6871fb6a220690375abd66d9576671e154f8269
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-06-28 09:00:59 -0300

    Add Author

---
+++ src/main/java/com/lukinhasssss/casa_do_codigo/entities/Author.java
@@ -0,0 +1,48 @@
+package com.lukinhasssss.casa_do_codigo.entities;
+
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_author")
+public class Author {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String description;
+    private Instant registeredAt = Instant.now();
+
+    public Author(String name, String email, String description) {
+        this.name = name;
+        this.email = email;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Instant getRegisteredAt() {
+        return registeredAt;
+    }
+
+}
