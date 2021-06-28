c8f13222849d36c6b97bc800d1914305fff02b70
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-06-28 09:09:51 -0300

    Database seed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into tb_author(name, email, description, registered_at) values('Alex Green', 'alex@gmail.com', 'Descrição do Alex', TIMESTAMP WITH TIME ZONE '2021-06-28T10:00:00.12345Z');
+insert into tb_author(name, email, description, registered_at) values('Ana Grey', 'ana@gmail.com', 'Descrição da Ana', TIMESTAMP WITH TIME ZONE '2021-06-28T11:00:00.12345Z');
+insert into tb_author(name, email, description, registered_at) values('Bob Brown', 'bob@gmail.com', 'Descrição do Bob', TIMESTAMP WITH TIME ZONE '2021-06-28T12:00:00.12345Z');
