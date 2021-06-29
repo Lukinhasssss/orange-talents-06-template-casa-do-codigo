3283fe4d95eabc7c1a627a6a6c3d27297010e4aa
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-06-29 15:51:20 -0300

    Book details

--- src/main/java/com/lukinhasssss/casa_do_codigo/controllers/BookController.java
+++ src/main/java/com/lukinhasssss/casa_do_codigo/controllers/BookController.java
@@ -1,10 +1,12 @@
 package com.lukinhasssss.casa_do_codigo.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import com.lukinhasssss.casa_do_codigo.dto.book.BookDetails;
 import com.lukinhasssss.casa_do_codigo.dto.book.BookDto;
 import com.lukinhasssss.casa_do_codigo.dto.book.BookForm;
 import com.lukinhasssss.casa_do_codigo.dto.book.BookListing;
@@ -16,6 +18,7 @@ import com.lukinhasssss.casa_do_codigo.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +41,16 @@ public class BookController {
     public List<BookListing> findAll() {
         List<Book> books = bookRepository.findAll();
         return BookListing.convertToDto(books);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookDetails> findById(@PathVariable Long id) {
+        Optional<Book> book = bookRepository.findById(id);
+
+        if (book.isPresent())
+            return ResponseEntity.ok(new BookDetails(book.get()));
+
+        return ResponseEntity.notFound().build();
     }
 
     @PostMapping
